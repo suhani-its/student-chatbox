1833431202db376c19f3f316b3f2d2f36a376c76
Suhani Singh <singhsuhanii9106@gmail.com>
2026-07-14 10:13:17 +0530

    fix: remove secret key

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.secret_key = "secret123"
 
 # ========== 1. API KEY SETUP ==========
 
-client = genai.Client(api_key="AQ.Ab8RN6IQeC36S1pDO-5CiPEfg7oVg-9pdInWW2qf69MAzMY0CAm")
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 # ========== 2. DATABASE SETUP ==========
 conn = sqlite3.connect('college.db', check_same_thread=False)
 c = conn.cursor()
